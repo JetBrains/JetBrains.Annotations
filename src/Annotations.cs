3a1031025b285c202428f4dde60f7ebfa81631e4
Alexander.Shvedov <alexander.shvedov@jetbrains.com>
2021-10-26 15:32:45 +0300

    Extend documentation a bit

--- src/Annotations.cs
+++ src/Annotations.cs
@@ -587,18 +587,21 @@ namespace JetBrains.Annotations
 
   /// <summary>
   /// Tells the code analysis engine if the parameter is completely handled when the invoked method is on stack.
-  /// If the parameter is a delegate, indicates that delegate is executed while the method is executed.
+  /// If the parameter is a delegate, indicates that delegate can only be invoked during method execution
+  /// (the delegate can be invoked zero or multiple times, but not stored to some field and invoked later,
+  /// when the containing method is no longer on the execution stack).
   /// If the parameter is an enumerable, indicates that it is enumerated while the method is executed.
-  /// If <see cref="RequireAwait"/> is true, the attribute is only applied under 'await' expression.
+  /// If <see cref="RequireAwait"/> is true, the attribute will only takes effect if the method invocation is located under the 'await' expression.
   /// </summary>
   [AttributeUsage(AttributeTargets.Parameter)]
   [Conditional("JETBRAINS_ANNOTATIONS")]
   public sealed class InstantHandleAttribute : Attribute
   {
-      /// <summary>
-      /// Limits the applicability of the attribute to calls under 'await' expression.
-      /// </summary>
-      public bool RequireAwait { get; set; }
+    /// <summary>
+    /// Require the method invocation to be used under the 'await' expression for this attribute to take effect on code analysis engine.
+    /// Can be used for delegate/enumerable parameters of 'async' methods.
+    /// </summary>
+    public bool RequireAwait { get; set; }
   }
 
   /// <summary>
